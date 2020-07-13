27fb86d86b8653b0c000892d0686ed86661071e8
keyoumao <58833084+keyoumao@users.noreply.github.com>
2020-07-12 23:50:58 -0400

    Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,7 +7,11 @@ summary(lm(mpg ~ MPG_Regression$"vehicle length" + MPG_Regression$"vehicle weigh
 #convert data frame into numeric matrix
 MPG_matrix <- as.matrix(MPG_Regression[,c("vehicle length","vehicle weight","spoiler angle","AWD","mpg")])
 cor(MPG_matrix)
-lm(mpg ~ MPG_Regression$"vehicle length" + MPG_Regression$"vehicle weight" + AWD,data=MPG_Regression)
-
-
+lm(mpg ~ MPG_Regression$"vehicle length", MPG_Regression) #create linear model
+summary(lm(mpg ~ MPG_Regression$"vehicle length", MPG_Regression)) #summarize linear model
+model <- lm(mpg ~ MPG_Regression$"vehicle length", MPG_Regression)  #create linear model
+yvals <- model$coefficients['MPG_Regression$"vehicle length"']*MPG_Regression$"vehicle length" +
+  model$coefficients['(Intercept)'] #determine y-axis values from linear model
+plt <- ggplot(MPG_Regression,aes(x=MPG_Regression$"vehicle length",y=mpg)) #import dataset into ggplot2
+plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter and linear model
 
